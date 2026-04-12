0ef0b7ec2b4b89b8e5eb4f21bed22da42a1977ad
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-04-12 13:52:26 +0200

    feat : implemented room , room type , amenity fully fix : fixed an issue with the payable interface

--- src/Files/Classes/Amenity.java
+++ src/Files/Classes/Amenity.java
@@ -1,5 +1,41 @@
 package Files.Classes;
-public class Amenity {
-    String amenityName;
-    String description;
+public class Amenity 
+{
+    private String amenityName;
+    private String description;
+    private double amenityPrice;
+
+    public Amenity() {}
+
+    public Amenity(String amenityName, String description, double amenityPrice) 
+    {
+        this.amenityName = amenityName;
+        this.description = description;
+        this.amenityPrice = amenityPrice;
+    }
+
+    public String getAmenityName() {
+        return amenityName;
+    }
+
+    public void setAmenityName(String amenityName) {
+        this.amenityName = amenityName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmenityPrice() {
+        return amenityPrice;
+    }
+
+    public void setAmenityPrice(double amenityPrice) {
+        this.amenityPrice = amenityPrice;
+    }
+
 }
--- src/Files/Classes/RoomType.java
+++ src/Files/Classes/RoomType.java
@@ -1,3 +1,54 @@
 package Files.Classes;
-public class RoomType {
+public class RoomType 
+{
+    private String typeName;
+    private double pricePerNight;
+    private String description;
+    private double seasonMultiplier;
+
+    public RoomType() {}
+
+    public RoomType(String typeName, double pricePerNight, String description, double seasonMultiplier) 
+    {
+        this.typeName = typeName;
+        this.pricePerNight = pricePerNight;
+        this.description = description;
+        this.seasonMultiplier = seasonMultiplier;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getSeasonMultiplier() {
+        return seasonMultiplier;
+    }
+
+    public void setSeasonMultiplier(double seasonMultiplier) {
+        this.seasonMultiplier = seasonMultiplier;
+    }
+
+    public double getEffectivePrice() {
+        return pricePerNight * seasonMultiplier;
+    }
 }
