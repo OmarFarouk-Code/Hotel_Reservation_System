8fdaf014f520cc2bd6621037a4320e2497a12ba8
adamelhessy <adam.elheessy2007@gmail.com>
2026-04-13 00:44:01 +0200

    added 3 functions to admin

--- src/hotel/core/Database.java
+++ src/hotel/core/Database.java
@@ -4,6 +4,7 @@ import hotel.model.staff.*;
 import hotel.model.users.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*; // Import this for File IO
 
 public class Database 
 {
@@ -23,6 +24,33 @@ public class Database
     public static List<Invoice> getInvoices() { return invoices; }
     public static List<RoomType> getRoomTypes() { return roomTypes; }
     public static List<Amenity> getAmenities() { return amenities; }
+    private static final String FILE_NAME = "hotel_data.dat";
+
+    // Call this whenever you change something (Create, Update, Delete)
+    public static void saveData() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(rooms);
+            oos.writeObject(amenities);
+            // Add other lists here
+            System.out.println("Data saved successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Call this ONCE when your program first starts
+    public static void loadData() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) return; // Nothing to load yet
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            rooms = (List<Room>) ois.readObject();
+            amenities = (List<Amenity>) ois.readObject();
+            // Load other lists in the SAME ORDER you saved them
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
     
