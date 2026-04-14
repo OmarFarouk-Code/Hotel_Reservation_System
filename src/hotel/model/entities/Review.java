5fcbe90fbe730e6c4f5a7c144dbe1d2a91c2c724
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-04-14 19:02:56 +0200

    feat : implemened room reviews class

---
+++ src/hotel/model/entities/Review.java
@@ -0,0 +1,30 @@
+package hotel.model.entities;
+
+public class Review 
+{
+    private int score;
+    private String comment;
+
+    public Review ( int score, String comment)
+    {
+        this.score = score;   
+        this.comment = comment;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+    
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+}
