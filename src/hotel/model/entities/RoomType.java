5f64e817a59cc8cf5c298934f7942b18e4dd9ec1
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-04-14 19:03:48 +0200

    implemented roomView enum and added it in room type class as an extra feature

--- src/hotel/model/entities/RoomType.java
+++ src/hotel/model/entities/RoomType.java
@@ -1,19 +1,24 @@
 package hotel.model.entities;
+import hotel.model.enums.*;
 public class RoomType 
 {
     private String typeName;
+    private RoomView roomView;
     private double pricePerNight;
     private String description;
     private double seasonMultiplier;
+    private int maxCapacity;
 
     public RoomType() {}
 
-    public RoomType(String typeName, double pricePerNight, String description, double seasonMultiplier) 
+    public RoomType(String typeName, RoomView roomView, double pricePerNight, String description, double seasonMultiplier , int maxCapacity ) 
     {
         this.typeName = typeName;
+        this.roomView = roomView;
         this.pricePerNight = pricePerNight;
         this.description = description;
         this.seasonMultiplier = seasonMultiplier;
+        this.maxCapacity = maxCapacity;
     }
 
     public String getTypeName() {
@@ -46,6 +51,22 @@ public class RoomType
 
     public void setSeasonMultiplier(double seasonMultiplier) {
         this.seasonMultiplier = seasonMultiplier;
+    }
+
+    private RoomView getRoomView() {
+        return roomView;
+    }
+
+    private void setRoomView(RoomView roomView) {
+        this.roomView = roomView;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
     }
 
     public double getEffectivePrice() {
